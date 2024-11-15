fe48b0c23646817d12749a8c2f9e3628a7bdcee8
Joseph Kato <joseph@jdkato.io>
2024-11-14 19:27:28 -0800

    WIP

---
+++ src/lib/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Vale CLI",
+    url: "https://vale.sh",
+    ogImage: "https://shadcn-svelte.com/og.png",
+    description: " A markup-aware linter for prose built with speed and extensibility in mind.",
+    links: {
+        github: "https://github.com/errata-ai/vale",
+        twitter: "https://twitter.com/jdkato",
+    },
+    keywords: `linting,nlp,cli`,
+};
+
+export type SiteConfig = typeof siteConfig;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import SiteHeader from '$lib/components/SiteHeader.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+<SiteHeader />
 {@render children()}
